0b3585f5f547d660d3dbed9f7bd2c49403326adc
Alex <alex.grabski555@gmail.com>
2024-09-04 20:38:49 +0200

    Utility class structure

---
+++ src/Utility/ResourceManager.h
@@ -0,0 +1,16 @@
+//
+// Created by alexg on 04-Sep-24.
+//
+
+#ifndef RESOURCEMANAGER_H
+#define RESOURCEMANAGER_H
+
+namespace fr {
+
+class ResourceManager {
+
+};
+
+} // fr
+
+#endif //RESOURCEMANAGER_H
---
+++ src/Utility/Style.h
@@ -0,0 +1,16 @@
+//
+// Created by alexg on 04-Sep-24.
+//
+
+#ifndef STYLE_H
+#define STYLE_H
+
+namespace fr {
+
+class Style {
+
+};
+
+} // fr
+
+#endif //STYLE_H
