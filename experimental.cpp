212dabb9a5089da191f50a3a39b466d95b5735b6
Alex-G-R <alex.grabski555@gmail.com>
2026-01-16 22:00:09 +0100

    Rewrite main to clear things out

---
+++ experimental.cpp
@@ -0,0 +1,92 @@
+#include "FRSGUI/FRSGUI.hpp"
+
+int main()
+{
+    /* initialize SFML render window, create a pointer to that render window */
+    constexpr int WINDOW_WIDTH = 1920;
+    constexpr int WINDOW_HEIGHT = 1080;
+    const auto RENDER_WINDOW_POINTER = std::make_shared<sf::RenderWindow>(sf::VideoMode(sf::Vector2u(WINDOW_WIDTH, WINDOW_HEIGHT)), "FRSGUI - Window | Alex G", sf::Style::Default);
+
+    /* initialize FRSGUI by passing the render window pointer */
+    fr::FRSGUI FRSGUI(RENDER_WINDOW_POINTER);
+
+    /* Create SFML elements here, custom non FRSGUI visuals */
+
+    /* Create GUI here */
+    // Create a banner element as a default UIElement
+    const auto banner = fr::UIElement::Builder(&FRSGUI)
+    .setID("Banner")
+    .addGroup("Banner_main")
+    .setText("Hey, Welcome to FRSGUI :)")
+    .buildUIElement();
+    // Create a style for Banner_main
+    auto banner_main_style = Style("Banner_main", fr::ApplyBy::GROUP, 1, {
+        {fr::KEY::POSITION, sf::Vector2f(WINDOW_WIDTH/2 - 500.f/2 , 50.f)},
+        {fr::KEY::SIZE, sf::Vector2f(500.f, 60.f)},
+        {fr::KEY::BACKGROUND_COLOR, sf::Color{0, 255, 0, 255}},
+        {fr::KEY::CHARACTER_SIZE, 36},
+        {fr::KEY::TEXT_COLOR, sf::Color::White}
+    }, FRSGUI);
+
+    // Create a button element as a fr::Button
+    const auto button = fr::Button::Builder(&FRSGUI)
+    .setID("button")
+    .addGroup("Button_main")
+    .setText("Click me!")
+    .buildButton();
+    // Create a style for button_main
+    const auto button_main_style = Style("Button_main", fr::ApplyBy::GROUP, 1, {
+            {fr::KEY::SIZE, sf::Vector2f(140.f, 100.f)},
+            {fr::KEY::POSITION, sf::Vector2f(WINDOW_WIDTH/6 - 140.f/2, 150.f)},
+            {fr::KEY::BACKGROUND_COLOR, sf::Color::Blue},
+            {fr::KEY::CHARACTER_SIZE, 30},
+            {fr::KEY::TEXT_COLOR, sf::Color::White},
+            {fr::KEY::OUTLINE_THICKNESS, 4.f},
+            {fr::KEY::OUTLINE_COLOR, sf::Color{160, 160, 255, 255}},
+    }, FRSGUI);
+    // Create functionality for the button
+    int banner_status = 0;
+    // Create custom background color style with higher priority
+    auto red_bg_style = Style("red_bg_style", fr::ApplyBy::GROUP, 5, {
+            {fr::KEY::BACKGROUND_COLOR, sf::Color{255, 0, 0, 255}},
+    }, FRSGUI);
+    button->setOnClick([&FRSGUI, &banner_status]()
+    {
+        if (banner_status % 2 == 0)
+        {
+            FRSGUI.getUIElementByID("Banner")->addGroup("red_bg_style");
+        }
+        else
+        {
+            FRSGUI.getUIElementByID("Banner")->deleteGroup("red_bg_style");
+        }
+        banner_status++;
+    });
+
+    /* SFML render loop */
+    while(RENDER_WINDOW_POINTER->isOpen())
+    {
+        /* Pool events, if event = close, break the loop if any other pass it to the FRSGUI to handle*/
+        while(const std::optional<sf::Event> event = RENDER_WINDOW_POINTER->pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                RENDER_WINDOW_POINTER->close();
+                break;
+            }
+            FRSGUI.dispatchEvent(event);
+        }
+
+        RENDER_WINDOW_POINTER->clear();
+
+        /* Custom rendering, custom non FRSGUI elements */
+
+
+        /* Render GUI by FRSGUI */
+        FRSGUI.Render();
+
+        /* SFML display function */
+        RENDER_WINDOW_POINTER->display();
+    }
+    return 0;
+}
