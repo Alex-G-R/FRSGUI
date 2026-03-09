2f786ef70a36606f6bb88ad88cd07357dc43dda0
Alex-G-R <alex.grabski555@gmail.com>
2026-03-09 20:35:16 +0100

    Change main

--- main.cpp
+++ main.cpp
@@ -1,184 +1,160 @@
 #include "FRSGUI/FRSGUI.hpp"
-#include <iostream>
 
 int main()
 {
-    const auto render_window_ptr = std::make_shared<sf::RenderWindow>(sf::VideoMode(sf::Vector2u(1920, 1080)), "FRSGUI test", sf::Style::Default);
+    /* initialize SFML render window, create a pointer to that render window */
+    constexpr int W_WIDTH = 1920;
+    constexpr int W_HEIGHT = 1080;
+    const auto WINDOW = std::make_shared<sf::RenderWindow>(sf::VideoMode({W_WIDTH, W_HEIGHT}), "FRSGUI - Window");
 
-    fr::FRSGUI frsgui(render_window_ptr);
+    /* initialize FRSGUI by passing the render window pointer */
+    fr::FRSGUI FRSGUI(WINDOW);
 
-    // Create GUI here
-    const auto square = fr::UIElement::Builder(&frsgui)
-    .setID("Foo")
-    .addGroups({"squares", "squares_two", "squares_three", "squares_four"})
-    .setText("Hello FRSGUI!")
+    /* Create SFML elements here, custom non FRSGUI visuals */
+
+    /* Create GUI here */
+    // Create a banner element as a default UIElement
+    const auto banner = fr::UIElement::Builder(&FRSGUI)
+    .setID("Banner")
+    .addGroup("Banner_main")
+    .setText("Hey, Welcome to FRSGUI :)")
     .buildUIElement();
-
-    auto squares_style = Style("squares", fr::ApplyBy::GROUP, 1,{
-        {fr::KEY::POSITION, sf::Vector2f(50.f, 50.f)},
-        {fr::KEY::SIZE, sf::Vector2f(500.f, 30.f)},
+    // Create a style for Banner_main
+    auto banner_main_style = Style("Banner_main", fr::ApplyBy::GROUP, 1, {
+        {fr::KEY::POSITION, sf::Vector2f(W_WIDTH/2 - 500.f/2 , 50.f)},
+        {fr::KEY::SIZE, sf::Vector2f(500.f, 60.f)},
         {fr::KEY::BACKGROUND_COLOR, sf::Color{0, 255, 0, 255}},
-        {fr::KEY::OUTLINE_THICKNESS, 10.f},
-        {fr::KEY::OUTLINE_COLOR, sf::Color{0, 255, 0, 255}},
-        {fr::KEY::VISIBLE, true},
-        {fr::KEY::CHARACTER_SIZE, 20},
-        {fr::KEY::TEXT_COLOR, sf::Color::Magenta},
-        {fr::KEY::FONT, "man"}
-    }, frsgui);
+        {fr::KEY::CHARACTER_SIZE, 36},
+        {fr::KEY::TEXT_COLOR, sf::Color::White}
+    }, FRSGUI);
 
-
-    auto squares_two = Style("squares_two", fr::ApplyBy::GROUP, 17, {
-        {fr::KEY::BACKGROUND_COLOR, sf::Color::Black}
-    }, frsgui);
-
-    auto squares_three = Style("squares_three", fr::ApplyBy::GROUP, 15, {
-        {fr::KEY::OUTLINE_COLOR, sf::Color::Red}
-    }, frsgui);
-
-    auto squares_four = Style("squares_four", fr::ApplyBy::GROUP, 7, {
-        {fr::KEY::BACKGROUND_COLOR, sf::Color::Yellow}
-    }, frsgui);
-
-    //button test
-    const auto btn = fr::Button::Builder(&frsgui)
-    .addGroup("btn")
-    .setText("Button")
+    // Create a button element as a fr::Button
+    const auto button = fr::Button::Builder(&FRSGUI)
+    .setID("button")
+    .addGroup("Button_main")
+    .setText("Click me!")
     .buildButton();
-
-    auto btn_style = Style("btn", fr::ApplyBy::GROUP, 1,{
-        {fr::KEY::SIZE, sf::Vector2f(100.f, 100.f)},
-        {fr::KEY::POSITION, sf::Vector2f(800.f, 300.f)},
-        {fr::KEY::BACKGROUND_COLOR, sf::Color::Cyan},
+    // Create a style for button_main
+    const auto button_main_style = Style("Button_main", fr::ApplyBy::GROUP, 1, {
+        {fr::KEY::SIZE, sf::Vector2f(140.f, 100.f)},
+        {fr::KEY::POSITION, sf::Vector2f(W_WIDTH/6 - 140.f/2, 150.f)},
+        {fr::KEY::BACKGROUND_COLOR, sf::Color::Blue},
         {fr::KEY::CHARACTER_SIZE, 30},
-        {fr::KEY::TEXT_COLOR, sf::Color::Black}
-    }, frsgui);
-
-    // testing custom event handlers
-    sf::RectangleShape player;
-    player.setFillColor(sf::Color::White);
-    player.setPosition(sf::Vector2f(800.f, 800.f));
-    player.setSize(sf::Vector2f(50.f, 50.f));
-
-    btn->setOnClick([&player, &square, &frsgui, &squares_style]()
+        {fr::KEY::TEXT_COLOR, sf::Color::White},
+        {fr::KEY::OUTLINE_THICKNESS, 4.f},
+        {fr::KEY::OUTLINE_COLOR, sf::Color{160, 160, 255, 255}},
+    }, FRSGUI);
+    // Create functionality for the button
+    int banner_status = 0;
+    // Create custom background color style with higher priority
+    auto red_bg_style = Style("red_bg_style", fr::ApplyBy::GROUP, 5, {
+            {fr::KEY::BACKGROUND_COLOR, sf::Color{255, 0, 0, 255}},
+    }, FRSGUI);
+    button->setOnClick([&FRSGUI, &banner_status]()
     {
-        // Object manipulation by capture
-        //square->deleteGroup("squares");
-
-        /* Object manipulation by FRSGUI capture */
-        frsgui.getUIElementByID("Foo")->deleteGroup("squares_two");
-
-        /* Custom functionality by capture */
-        //std::cout << "Button clicked. \n";
-        // Player will move only when checkbox is selected
-        //(frsgui.getCheckboxByID("checkbox_id")->getSelect() == true) works the same as (reinterpret_cast<std::shared_ptr<fr::Checkbox>&>(frsgui.getUIElementByID("checkbox_id"))->getSelect() == true)
-        if(frsgui.getCheckboxByID("checkbox_id")->getSelect() == true)
+        /* Change the banner color */
+        if (banner_status % 2 == 0)
         {
-            player.move(sf::Vector2f(10.f, 0.f));
+            FRSGUI.getUIElementByID("Banner")->addGroup("red_bg_style");
         }
+        else
+        {
+            FRSGUI.getUIElementByID("Banner")->deleteGroup("red_bg_style");
+        }
+        banner_status++;
 
-        /* Styles manipulation by style capture */
-        squares_style->updateStyle({
-            {fr::KEY::BACKGROUND_COLOR, sf::Color::Yellow},
-            {fr::KEY::OUTLINE_THICKNESS, 2}
-        });
-
-        // Size debug |
-        // std::cout << "Size of element: " << sizeof(square) << "\n";
-        // std::cout << "Size of frsgui: " << sizeof(frsgui) << "\n";
-
-        // Operation on data from inputs
-        const auto num1 = frsgui.getInputByID("num1");
-        const auto num2 = frsgui.getInputByID("num2");
-        const auto addition_result = num1->getDataAs<double>() + num2->getDataAs<double>();
-
-        // You can pass numbers now!
-        frsgui.getUIElementByID("result")->setText(addition_result);
+        /* Handle operation on inputs if checkbox is selected */
+        if (FRSGUI.getCheckboxByID("checkbox_A")->getSelect() == true)
+        {
+            const auto num1 = FRSGUI.getInputByID("input_A");
+            const auto num2 = FRSGUI.getInputByID("input_B");
+            const auto result = num1->getDataAs<double>() + num2->getDataAs<double>();
+            // Display the result
+            FRSGUI.getUIElementByID("result")->setText(result);
+        }
     });
 
-    //Bool is_numerical_only is an overload, defaults to false
-    const auto input = fr::Input::Builder(&frsgui)
-    .addGroup("input")
+    /* Create two numer inputs */
+    const auto input_A = fr::Input::Builder(&FRSGUI)
+    .addGroup("input_main")
     .isNumericalOnly(true)
-    .setID("num1")
+    .setID("input_A")
+    .buildInput();
+    const auto input_B = fr::Input::Builder(&FRSGUI)
+    .addGroup("input_main")
+    .isNumericalOnly(true)
+    .setID("input_B")
     .buildInput();
 
-    auto input_style = Style("input", fr::ApplyBy::GROUP, 1, {
+    /* Add styling */
+    auto input_main_style = Style("input_main", fr::ApplyBy::GROUP, 1, {
         {fr::KEY::SIZE, sf::Vector2f(300.f, 60.f)},
-        {fr::KEY::POSITION, sf::Vector2f(500.f, 500.f)},
-        {fr::KEY::OUTLINE_THICKNESS, 1},
-        {fr::KEY::OUTLINE_COLOR, sf::Color(160, 160, 160)},
-        {fr::KEY::BACKGROUND_COLOR, sf::Color(35, 34, 33)},
-        // ended here
+        {fr::KEY::OUTLINE_THICKNESS, 2},
+        {fr::KEY::OUTLINE_COLOR, sf::Color{160, 160, 160, 255}},
+        {fr::KEY::BACKGROUND_COLOR, sf::Color::White},
         {fr::KEY::CURSOR_COLOR, sf::Color(133, 134, 135, 255)},
-        {fr::KEY::CHARACTER_SIZE, 50}
-    }, frsgui);
+        {fr::KEY::CHARACTER_SIZE, 50},
+        {fr::KEY::TEXT_COLOR, sf::Color::Black}
+    }, FRSGUI);
+    auto input_A_style = Style("input_A", fr::ApplyBy::ID, 1, {
+        {fr::KEY::POSITION, sf::Vector2f(500.f, 500.f)}
+    }, FRSGUI);
+    auto input_B_style = Style("input_B", fr::ApplyBy::ID, 1, {
+        {fr::KEY::POSITION, sf::Vector2f(900.f, 500.f)}
+    }, FRSGUI);
 
-    const auto inputTwo = fr::Input::Builder(&frsgui)
-    .addGroup("input")
-    .addGroup("inputTwo")
-    .isNumericalOnly(true)
-    .setID("num2")
-    .buildInput();
-
-    auto input_style_two = Style("inputTwo", fr::ApplyBy::GROUP, 2, {
-        {fr::KEY::POSITION, sf::Vector2f(900.f, 500.f)},
-    }, frsgui);
-
-    // Add fonts
-    frsgui.addFont("man", "./fonts/man.ttf");
-
-    // add textblock
-    const auto result = fr::TextBlock::Builder(&frsgui)
-    .addGroup("textblock_style")
+    /* Create a result block for addition */
+    const auto result = fr::TextBlock::Builder(&FRSGUI)
+    .addGroup("result_main")
     .setID("result")
     .buildTextBlock();
 
-    auto textblock_style = Style("textblock_style", fr::ApplyBy::GROUP, 1, {
-        {fr::KEY::CHARACTER_SIZE, 64},
-        {fr::KEY::TEXT_COLOR, sf::Color::Blue},
-        {fr::KEY::POSITION, sf::Vector2f(700.f, 700.f)},
+    auto textblock_style = Style("result_main", fr::ApplyBy::GROUP, 1, {
+        {fr::KEY::CHARACTER_SIZE, 50},
+        {fr::KEY::TEXT_COLOR, sf::Color::White},
+        {fr::KEY::POSITION, sf::Vector2f(1300.f, 490.f)},
         {fr::KEY::SIZE, sf::Vector2f(300.f, 80.f)},
         {fr::KEY::BACKGROUND_COLOR, sf::Color::Black}
-    }, frsgui);
+    }, FRSGUI);
 
-    // Test checkbox
-    const auto checkbox = fr::Checkbox::Builder(&frsgui)
-    .addGroup("checkbox_style")
-    .setID("checkbox_id")
+    /* Create a checkbox */
+    const auto checkbox = fr::Checkbox::Builder(&FRSGUI)
+    .addGroup("checkbox_main")
+    .setID("checkbox_A")
     .buildCheckbox();
-
-    auto checkbox_style = Style("checkbox_style", fr::ApplyBy::GROUP, 1, {
+    auto checkbox_style = Style("checkbox_main", fr::ApplyBy::GROUP, 1, {
         {fr::KEY::POSITION, sf::Vector2f(1700.f, 200.f)},
         {fr::KEY::SIZE, sf::Vector2f(100.f, 100.f)},
         {fr::KEY::BACKGROUND_COLOR, sf::Color::White},
         {fr::KEY::OUTLINE_COLOR, sf::Color(134, 135, 136)},
-        {fr::KEY::OUTLINE_THICKNESS, 8.f},
-        {fr::KEY::SELECT_MARK_COLOR, sf::Color(134, 135, 136)}
-    }, frsgui);
+        {fr::KEY::OUTLINE_THICKNESS, 2.f},
+        {fr::KEY::SELECT_MARK_COLOR, sf::Color(0, 0, 0)}
+    }, FRSGUI);
 
-    while(render_window_ptr->isOpen())
+    /* SFML render loop */
+    while(WINDOW->isOpen())
     {
-        while(const std::optional<sf::Event> event = render_window_ptr->pollEvent())
+        /* Pool events, if event = close, break the loop if any other pass it to the FRSGUI to handle*/
+        while(const std::optional<sf::Event> event = WINDOW->pollEvent())
         {
-            if(event->is<sf::Event::Closed>())
+            if (event->is<sf::Event::Closed>())
             {
-                render_window_ptr->close();
+                WINDOW->close();
                 break;
             }
-            frsgui.dispatchEvent(event);
+            FRSGUI.dispatchEvent(event);
         }
 
-        render_window_ptr->clear();
+        WINDOW->clear();
 
-        // Your rendering
-        render_window_ptr->draw(player);
+        /* Custom rendering, custom non FRSGUI elements */
 
-        // Render GUI at the end
-        frsgui.Render();
 
-        // Display
-        render_window_ptr->display();
+        /* Render GUI by FRSGUI */
+        FRSGUI.Render();
+
+        /* SFML display function */
+        WINDOW->display();
     }
-
     return 0;
 }
